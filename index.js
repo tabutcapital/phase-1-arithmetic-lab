0928493999ce5aa072de37b16f335036fbc04a06
tabutcapital <iantabut3@gmail.com>
2024-10-23 18:32:38 +0300

    arithmetic labs

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 22;
+let num4 = 9;
+const mod = num3 % num4;
+
+const max = Math.max(2,5,8,9,20)
